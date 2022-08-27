74c4f538a4f4950a09ac840873892dc163f8d568
Pilevich Sasha <pilevichsasha@gmail.com>
2022-08-27 23:35:58 +0300

    fix task

--- react-hw-1/src/components/Button/index.tsx
+++ react-hw-1/src/components/Button/index.tsx
@@ -1,16 +1,33 @@
-import style from "./style.module.css"
+import style from "./style.module.css";
 
 interface IButton {
-    label: string;
-    onClick:() => void;
-    disabled: boolean;
+  label: string;
+  onClick: () => void;
+  disabled: boolean;
+  type: "primary" | "secondary" | "secondary2";
 }
 
-export const Button = (props:IButton) => {
-    return(
-        <div className={style.buttonContainer}>
-            <button className={style.button} disabled={props.disabled} onClick={props.onClick}>{props.label}</button>
-        </div>
-        
-    )
-}
+const getButtonStyle = (type: "primary" | "secondary" | "secondary2") => {
+  if (type === "primary") {
+    return style.primary;
+  }
+  if (type === "secondary") {
+    return style.secondary;
+  }
+  if (type === "secondary2") {
+    return style.secondary2;
+  }
+};
+export const Button = (props: IButton) => {
+  return (
+    <div className={style.buttonContainer}>
+      <button
+        className={`${style.button} ${getButtonStyle(props.type)}`}
+        disabled={props.disabled}
+        onClick={props.onClick}
+      >
+        {props.label}
+      </button>
+    </div>
+  );
+};
